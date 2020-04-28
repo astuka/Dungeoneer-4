6c31143b32c945c5309b2419f4cd6b520cae5221
astuka <jrobins855@gmail.com>
2020-04-28 08:27:57 -0700

    Implemented defense attribute

--- dungeoneer4.py
+++ dungeoneer4.py
@@ -1,11 +1,13 @@
 import random as r
 
+#creates a Character
 class Character:
     def __init__(self,health=100,atk=10,de=10):
         self.health = health
         self.atk = atk
         self.de = de
 
+#creates a Monster
 class Monster:
     def __init__(self,name, health,atk,de):
         self.name = name
@@ -13,34 +15,41 @@ class Monster:
         self.atk = atk
         self.de = de
 
+#main menu
 test = input("Welcome to Dungeoneer 4\n1. Start game \n")
 
-goblin = Monster("Goblin", 10, 3, 3)
-orc = Monster("Orc", 5, 5, 5)
-ogre = Monster("Ogre", 20, 1, 1)
-
+#monster initialization
+goblin = Monster("Goblin", 10, 13, 3)
+orc = Monster("Orc", 5, 15, 5)
+ogre = Monster("Ogre", 20, 11, 1)
 monsters = [goblin,orc,ogre]
 
+#game loop
 game = True
-
 if test == "1":
     player = Character()
     while game:
+        #pick monster
         monster = monsters[r.randrange(0,len(monsters))]
+        #make sure monsters health doesnt save
+        monster_default = monster.health
         fight = True
         while fight: 
             ans = input("You are fighting a "+monster.name+". What would you like to do?\n1.Attack\n")
             if ans == "1":
-                monster.health -= r.randrange(0,player.atk)
-                player.health -= r.randrange(0,monster.atk)
+                monster.health -= max(r.randrange(0,player.atk) - r.randrange(0,monster.de),0) #either does damage or blocks all damage
+                player.health -= max(r.randrange(0,monster.atk) - r.randrange(0,player.de),0)
                 if monster.health <= 0:
                     print("You killed the "+monster.name)
                     fight = False
+                    #reset monster health
+                    monster.health = monster_default
+                else:
+                    print(monster.name+" is now at "+str(monster.health)+". You are now at "+str(player.health)+"\n")
                 if player.health <= 0:
                     print("You died.")
                     fight = False
                     game = False
-                print(monster.name+" is now at "+str(monster.health)+". You are now at "+str(player.health)+"\n")
 else:
     while test != "1":
         test = input("Please choose a valid option.\n")
