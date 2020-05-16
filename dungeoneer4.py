9383ea580cb07e841dac24a8194d1026d6fbeba4
astuka <jrobins855@gmail.com>
2020-05-16 15:19:36 +0000

    Added save feature

--- dungeoneer4.py
+++ dungeoneer4.py
@@ -1,4 +1,5 @@
 #TO DO
+#save feature
 #the first enemy you fight in a zone is an enemy from the old zone, then it goes to the new zone. why? 
 #incorporate elements from DnD
 
@@ -25,7 +26,7 @@ class Monster:
         self.xp = xp
 
 #main menu
-test = input("Welcome to Dungeoneer 4\n1. Start game \n")
+test = input("Welcome to Dungeoneer 4\n1. Start game \n2. Load Game\n")
 
 #monster initialization
 goblin = Monster("Goblin", 10, 13, 3, 10)
@@ -45,51 +46,60 @@ monsters = lake_monsters
 #game loop
 game = True
 if test == "1":
-    player = Character()
-    player_default = player.health
-    while game:
-        #pick monster
-        monster = monsters[r.randrange(0,len(monsters))] #fix this this no longer works
-        #make sure monsters health doesnt save
-        monster_default = monster.health
-        fight = True
-        while fight: 
-            ans = input("You are fighting a "+monster.name+". What would you like to do?\n1.Attack\n2.Switch Zone\n")
-            if ans == "1":
-                monster.health -= max(r.randrange(0,player.atk) - r.randrange(0,monster.de),0) #either does damage or blocks all damage
-                player.health -= max(r.randrange(0,monster.atk) - r.randrange(0,player.de),0)
-                if monster.health <= 0:
-                    print("You killed the "+monster.name+" and received "+str(monster.xp)+" XP.")
-                    fight = False
-                    player.xp += monster.xp
-                    if player.xp >= player.xp_cap:
-                        player.lvl += 1
-                        player.xp = 0
-                        player.health = player_default + 10
-                        player_default = player.health 
-                        player.atk += 1
-                        player.de += 1
-                        player.xp_cap += 50
-                        print("You leveled up! You are now level "+str(player.lvl))
-                    #reset monster health
-                    monster.health = monster_default
-                else:
-                    print(monster.name+" is now at "+str(monster.health)+". You are now at "+str(player.health)+"\n")
-                if player.health <= 0:
-                    print("You died.")
-                    fight = False
-                    game = False
-            if ans == "2":
-                num_zone = 1
-                for zone in zones:
-                    print(str(num_zone) +". "+ zone+"\n")
-                    num_zone += 1
-                zone_choice = input("Which zone would you like to go to?\n")
-                print(zones[int(zone_choice) - 1])
-                if zone_choice == '1':
-                    monsters = lake_monsters    
-                if zone_choice == '2':
-                    monsters = forest_monsters             
+  player = Character()
+  player_default = player.health
+
+if test == "2":
+  player = np.load('player.npy')
+  player_default = player.health
+    
+while game:
+    #pick monster
+    monster = monsters[r.randrange(0,len(monsters))] #fix this this no longer works
+    #make sure monsters health doesnt save
+    monster_default = monster.health
+    fight = True
+    while fight: 
+        ans = input("You are fighting a "+monster.name+". What would you like to do?\n1.Attack\n2.Switch Zone\n3.Save\n")
+        if ans == "1":
+            monster.health -= max(r.randrange(0,player.atk) - r.randrange(0,monster.de),0) #either does damage or blocks all damage
+            player.health -= max(r.randrange(0,monster.atk) - r.randrange(0,player.de),0)
+            if monster.health <= 0:
+                print("You killed the "+monster.name+" and received "+str(monster.xp)+" XP.")
+                fight = False
+                player.xp += monster.xp
+                if player.xp >= player.xp_cap:
+                    player.lvl += 1
+                    player.xp = 0
+                    player.health = player_default + 10
+                    player_default = player.health 
+                    player.atk += 1
+                    player.de += 1
+                    player.xp_cap += 50
+                    print("You leveled up! You are now level "+str(player.lvl))
+                #reset monster health
+                monster.health = monster_default
+            else:
+                print(monster.name+" is now at "+str(monster.health)+". You are now at "+str(player.health)+"\n")
+            if player.health <= 0:
+                print("You died.")
+                fight = False
+                game = False
+        if ans == "2":
+            num_zone = 1
+            for zone in zones:
+                print(str(num_zone) +". "+ zone+"\n")
+                num_zone += 1
+            zone_choice = input("Which zone would you like to go to?\n")
+            print(zones[int(zone_choice) - 1])
+            if zone_choice == '1':
+                monsters = lake_monsters    
+            if zone_choice == '2':
+                monsters = forest_monsters   
+        if ans == "3":
+          np.save('player',player)
+          print("Player saved.\n")
+
 else:
     while test != "1":
         test = input("Please choose a valid option.\n")
